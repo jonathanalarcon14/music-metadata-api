86ab1d27a6f50b2da22042d0f8ae021b8c38430d
Jonathan Alarcón <jonathanalarcon144@gmail.com>
2026-07-17 15:40:44 -0500

    fix: add max length validation to name and artist fields

--- src/songs/dto/song-request.dto.ts
+++ src/songs/dto/song-request.dto.ts
@@ -1,20 +1,24 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, MaxLength, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SongRequestDto {
   @ApiProperty({
     description: 'Song name',
     example: 'Blinding Lights',
+    maxLength: 100,
   })
   @IsString()
   @MinLength(2)
+  @MaxLength(100)
   name!: string;
 
   @ApiProperty({
     description: 'Artist name',
     example: 'The Weeknd',
+    maxLength: 100,
   })
   @IsString()
   @MinLength(2)
+  @MaxLength(100)
   artist!: string;
 }
